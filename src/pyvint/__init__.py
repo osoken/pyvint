83e5b4333c8ce0d8084dbc2ebbfd0d40e72e2f89
osoken <osoken@osoekawalab.com>
2024-07-04 00:05:50 +0900

    docs: add documentation link

--- src/pyvint/__init__.py
+++ src/pyvint/__init__.py
@@ -1,5 +1,5 @@
 from .core import decode, decode_stream, encode, read_vint
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 __all__ = ["decode", "decode_stream", "encode", "read_vint", "__version__"]
