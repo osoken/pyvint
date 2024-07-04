c76da76570e2855288bbcf04a3f94aeaa5a0d420
osoken <osoken@osoekawalab.com>
2024-07-04 23:10:24 +0900

    release: stable release

--- src/pyvint/__init__.py
+++ src/pyvint/__init__.py
@@ -1,5 +1,5 @@
 from .core import decode, decode_stream, encode, read_vint
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 __all__ = ["decode", "decode_stream", "encode", "read_vint", "__version__"]
